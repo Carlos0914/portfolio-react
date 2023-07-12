68ab3b09e36a6aeb9d9dc5ebb9447a64e22d3bf3
Carlos Lopez <carloslop0914@gmail.com>
2023-07-12 17:38:31 -0600

    Add menu on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,18 @@ import History from "./pages/History";
 import Projects from "./pages/Projects";
 import Random from "./pages/Random";
 import Contact from "./pages/Contact";
+import { SwipeableDrawer } from "@mui/material";
+import { useState } from "react";
 
 function App() {
+  const [showDrawer, setShowDrawer] = useState(false);
+
   return (
     <>
       <BrowserRouter>
-        <Navbar />
+        <div className="desktopNav">
+          <Navbar />
+        </div>
         <Routes>
           <Route path="/" Component={AboutMe} />
           <Route path="/history" Component={History} />
@@ -19,6 +25,21 @@ function App() {
           <Route path="/random" Component={Random} />
           <Route path="/contact" Component={Contact} />
         </Routes>
+        {/* TODO: Swipe area doesn't work on desktop */}
+        <SwipeableDrawer
+          anchor="top"
+          open={showDrawer}
+          onClose={() => setShowDrawer(false)}
+          onDoubleClick={() => setShowDrawer(true)}
+          onOpen={() => setShowDrawer(true)}
+          PaperProps={{ style: { backgroundColor: "transparent" } }}
+          SwipeAreaProps={{
+            className: "mobileNav",
+          }}
+          allowSwipeInChildren={true}
+        >
+          <Navbar />
+        </SwipeableDrawer>
       </BrowserRouter>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import { styled } from "@mui/system";
 import NavButton from "./NavButton";
 import { Computer, Dashboard, Mail, Person, Work } from "@mui/icons-material";
 import { useLocation } from "react-router-dom";
+import { isMobile } from "react-device-detect";
 
 const NavContainer = styled("div")({
-  minWidth: 'calc(320px - 8rem)',
+  minWidth: "calc(320px - 8rem)",
   borderRadius: 40,
-  display: "flex",
+  display: "block",
   flexDirection: "column",
   minHeight: 350,
   maxHeight: "calc(100% - 2rem)",
@@ -18,8 +19,23 @@ const NavContainer = styled("div")({
   border: "1px solid #969696",
   boxShadow: "8px 8px 8px #868686",
   "@media (max-width: 759px)": {
-    display: 'none'
-  }
+    borderRadius: "0 0 40px 40px",
+    boxShadow: "none",
+    paddingTop: "1rem",
+  },
+});
+
+const DragUpIcon = styled("p")({
+  position: "absolute",
+  bottom: 0,
+  margin: "2px 0",
+  textAlign: "center",
+  width: "calc(100% - 4rem)",
+  "&::before": {
+    content: '"»"',
+    display: "inline-block",
+    transform: "rotate(-90deg)",
+  },
 });
 
 const Navbar = () => {
@@ -57,6 +73,7 @@ const Navbar = () => {
         icon={<Mail />}
         active={location.pathname === "/contact"}
       />
+      {isMobile && <DragUpIcon />}
     </NavContainer>
   );
 };
