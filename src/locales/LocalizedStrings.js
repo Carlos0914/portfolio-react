a727229da4a471c4d96b059242dc1244c118ef1f
Carlos Lopez <carloslop0914@gmail.com>
2023-08-25 13:17:06 -0600

    Fix types

--- src/locales/LocalizedStrings.js
+++ src/locales/LocalizedStrings.js
@@ -165,7 +165,7 @@ export const strings = new LocalizedStrings({
     },
     home: {
       line1:
-        "¡Bienvenido a mi sitio personal! Mi nombre es Carlos López, soy un Licenciado en Matemáticas Aplicadas que desarrolló un gran interes en la programación muy temprano durante mi carrera, llevandome a aventurarme en el mercado de desarrollo de sotware.",
+        "¡Bienvenido a mi sitio personal! Mi nombre es Carlos López, soy un Licenciado en Matemáticas Aplicadas que desarrolló un gran interes en la programación muy temprano durante mi carrera, llevandome a aventurarme en el mercado de desarrollo de software.",
       line2:
         "Con más de tres años de experiencia, me especializo en el desarrollo web full-stack con un mayor enfoque en el backend. Me apasiona mantenerme al día con las últimas novedades en tecnología y expandir mi conocimiento continuamente.",
       line3:
@@ -263,7 +263,7 @@ export const strings = new LocalizedStrings({
     },
     contact: {
       headline:
-        "Si tienes alguna pregunta o quieres discutir alguna idea, puedes enviarme un mensage utlizando el formulario a continuación",
+        "Si tienes alguna pregunta o quieres discutir alguna idea, puedes enviarme un mensaje utlizando el formulario a continuación",
       send: "Enviar",
       contactLinkedin: "Si lo deseas, también puedes contactarme via {0}",
       nameLabel: "Nombre",
